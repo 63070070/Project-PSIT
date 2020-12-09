58ed483a34f9ac2bebc9783869d36234efc469e7
Jessada63070023 <73677830+Jessada63070023@users.noreply.github.com>
2020-12-09 14:27:39 +0700

    Update test project.py

--- test project.py
+++ test project.py
@@ -2,6 +2,7 @@
 import tkinter as tk
 from tkinter import messagebox
 from tkinter import StringVar
+from tkinter import *
 import random
 import os
 
@@ -22,21 +23,21 @@ def newgame():
     already_guess = []
 
 
-def guess(input_txt):
+def guess(*args):
     """Check Function"""
     global word_withspaces
     global number_guess
     global already_guess
-    input_txt = input_txt.get().rstrip().lower()
+    input_txt = blank_txt.get().rstrip().lower()
     txt = list(word_withspaces)
     guessed = list(lblword.get())
 
     if len(input_txt) > 1:
-        tk.messagebox.showerror('Warning!!', 'Please type only one letter')
+        tk.messagebox.showerror('Warning!!', 'Please type only one word')
     elif not input_txt.isalpha():
-        tk.messagebox.showerror('Warning!!', 'Please type a letter')
+        tk.messagebox.showerror('Warning!!', 'Please type a word')
     elif input_txt in already_guess:
-        tk.messagebox.showwarning("Warning!!!", "You have already guessed this letter")
+        tk.messagebox.showwarning("Warning!!!", "You have already guessed this word")
     else:
         already_guess += [input_txt]
         if word_withspaces.count(input_txt) > 0:
@@ -66,54 +67,71 @@ def guess(input_txt):
 window_1 = tk.Tk()
 window_1.title("Hangman!!!")
 
+
 word_list = ["car", "dog", "cat", "bloom", "book", "belt", "microwave", "gloves", "clock", "chair", "pencil", \
-             "knife", "silent", "majority", "elevator"]
-hint_list = ["It's a four-wheeled vehicle", "It can bark", "It's like a tiger but smaller", "Sweep things on the ground", \
-             "You can read it or write it", "Keeping your pants on your waist", "Heating things up", \
-             "Cover your hands", "It's a device for showing time", "Used for sit", \
-             "Used for writing or drawing", "It's used for cutting", \
-             "When thing goes quiet", "Most", "Used for lifting people"]
+             "knife", "silent", "majority", "elevator", "arrow", "ambulance", "drink", "star", "ability", \
+             "sticker", "sorrow", "soccer", "bottle", "phone", "road", "switch", "polite", "umbrella", "century", \
+             "school", "behavior", "current", "day", "death", "father", "flight", "look", "meat", "rain", "religion"]
+hint_list = ["It's a four-wheeled vehicle", "It can bark", "It's like a tiger but smaller", "sweep things on the ground", \
+             "you can read it or write it", "keeping your pants on your waist", "heating things up", \
+             "cover your hands", "a device for showing time", "used for sit", "used for writing or drawing", "used for cutting", \
+             "when thing goes quiet", "most", "used for lifting people", "Used with a bow", "Used for receiving patients", \
+             "when you thristy", "shinning in the space", "skill", "sticky paper", "sadness", \
+             "american football", "used for filling water", "used for calling", "a place where cars run", "on and off", \
+             "As opposed to rude", "used for protection agianst rain", "a period hundred years", "a place where people goes to study", \
+             "the way that someone behaves", "of the present time", "a period of 24 hours", "the end of life", "a male parent", \
+             "a journey in an aircraft", "to direct your eyes in order to see", "the flesh of an animal when it is used for food", \
+             "drops of water from clouds", "the belief in, and worship of, a god or gods"]
 
 path = os.getcwd()
-photos = [tk.PhotoImage(file=path+r"\images\hang4.png")\
-    , tk.PhotoImage(file=path+r'\images\hang5.png')\
-    , tk.PhotoImage(file=path+r'\images\hang6.png')\
-    , tk.PhotoImage(file=path+r'\images\hang7.png')\
-    , tk.PhotoImage(file=path+r'\images\hang8.png')\
-    , tk.PhotoImage(file=path+r'\images\hang9.png')\
-    , tk.PhotoImage(file=path+r'\images\hang10.png')\
-    , tk.PhotoImage(file=path+r'\images\hang11.png')]
+
+photos = [tk.PhotoImage(file=path+r"\images\new hang1_removebg.png").subsample(2, 2)\
+    , tk.PhotoImage(file=path+r'\images\new hang2_removebg.png').subsample(2, 2)\
+    , tk.PhotoImage(file=path+r'\images\new hang3_removebg.png').subsample(2, 2)\
+    , tk.PhotoImage(file=path+r'\images\new hang4_removebg.png').subsample(2, 2)\
+    , tk.PhotoImage(file=path+r'\images\new hang5_removebg.png').subsample(2, 2)\
+    , tk.PhotoImage(file=path+r'\images\new hang6_removebg.png').subsample(2, 2)\
+    , tk.PhotoImage(file=path+r'\images\new hang7_removebg.png').subsample(2, 2)\
+    , tk.PhotoImage(file=path+r'\images\new hang8_removebg.png').subsample(2, 2)\
+    , tk.PhotoImage(file=path+r'\images\enter btn.png').subsample(2)]
+
 
 window_1.option_add("*Font", "Consolas 40")
 width_sc, height_sc = window_1.winfo_screenwidth(), window_1.winfo_screenheight()
 window_1.geometry("%dx%d"%(width_sc, height_sc))
-tk.Label(window_1, text="Welcome to Hangman Minigame!!!", bg="#61F3EB")\
-    .place(anchor="center", x=width_sc/2, y=50)
+window_1.state('zoom')
+tk.Label(window_1, text="Welcome to Hangman Minigame!!!", bg="#61F3EB", font=("Forte", 60))\
+    .pack(side="top", pady=(height_sc//30, 0))
 
 
-imgLabel = tk.Label(window_1)
-imgLabel.place(anchor="center", x=width_sc/2, y=height_sc/4)
+frame_image = tk.Frame(window_1)
+imgLabel = tk.Label(frame_image)
 imgLabel.config(image=photos[0])
+imgLabel.pack()
+frame_image.pack(side="top", pady=(height_sc//30, 0))
 
 
 lblword = StringVar()
 txt_show = tk.Label(window_1, textvariable=lblword)
-txt_show.place(anchor="center", x=width_sc/2, y=400)
+txt_show.pack(side="top", pady=(height_sc//20, 0))
 
 
 word_hint = StringVar()
-txt_hint = tk.Label(window_1, textvariable=word_hint, bg="yellow")
-txt_hint.place(anchor="center", x=width_sc/2, y=525)
+txt_hint = tk.Label(window_1, textvariable=word_hint, bg="yellow", font=("Comic Sans MS", 40))
+txt_hint.pack(side="top", pady=(height_sc//20, 0))
 
 
-tk.Label(text="Enter your guess : ", bg="pink").place(x=200, y=650, anchor="w")
+frame_guess = tk.Frame(window_1)
+tk.Label(frame_guess, text="Enter your guess : ", bg="pink", font=("Comic Sans MS", 40)).pack(side="left", padx=(0, 10))
 blank_txt = StringVar()
-txt = tk.Entry(textvariable=blank_txt, width=10)
-txt.place(anchor="w", x=750, y=650)
+txt = tk.Entry(frame_guess, textvariable=blank_txt, width=4, bd=5, justify="center")
+txt.bind("<Return>", guess)
+txt.pack(side="left", padx=(0, 10))
 
 
-enter = tk.Button(window_1, text="Enter", font="Consolas 30", fg="#FF3232", width=10, command=lambda txt=txt: guess(txt))
-enter.place(anchor="center", x=1200, y=650)
+enter = tk.Button(frame_guess, image=photos[8], text="Enter", command=guess, borderwidth=0)
+enter.pack(side="left", padx=(10, 0))
+frame_guess.pack(side="top", pady=(height_sc//20, 0))
 
 newgame()
 window_1.mainloop()
